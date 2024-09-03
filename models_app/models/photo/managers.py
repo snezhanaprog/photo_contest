647f2b82309f18662d374446268b409ab440e1ff
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-03 12:42:35 +0300

    change manager photo

--- models_app/models/photo/managers.py
+++ models_app/models/photo/managers.py
@@ -1,5 +1,20 @@
 from django.db import models
-from .querysets import PhotoQuerySet
+from django.db.models import Q
+
+
+class PhotoQuerySet(models.QuerySet):
+    def search(self, query):
+        return self.filter(Q(name__icontains=query) |
+                           Q(description__icontains=query))
+
+    def filter_by_status(self, status):
+        return self.filter(status=status)
+
+    def filter_by_author(self, author):
+        return self.filter(author=author)
+
+    def sort_by_field(self, field):
+        return self.order_by(field)
 
 
 class PhotoManager(models.Manager):
@@ -10,22 +25,10 @@ class PhotoManager(models.Manager):
         return self.get_queryset().search(query)
 
     def filter_by_status(self, status):
-        return self.get_queryset().filter_by_status(status)
+        return self.get_queryset().filter_by_status(status=status)
 
-    def sort_by_count_comments_up(self):
-        return self.get_queryset().sort_by_count_comments_up()
+    def filter_by_author(self, author):
+        return self.get_queryset().filter_by_author(author=author)
 
-    def sort_by_count_comments_down(self):
-        return self.get_queryset().sort_by_count_comments_down()
-
-    def sort_by_count_voices_up(self):
-        return self.get_queryset().sort_by_count_voices_up()
-
-    def sort_by_count_voices_down(self):
-        return self.get_queryset().sort_by_count_voices_down()
-
-    def sort_by_date_publicated_up(self):
-        return self.get_queryset().sort_by_date_publicated_up()
-
-    def sort_by_date_publicated_down(self):
-        return self.get_queryset().sort_by_date_publicated_down()
+    def sort_by_field(self, field):
+        return self.get_queryset().sort_by_field(field=field)
