3397f88e4c548282bc52da582878db2517f8b9e0
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-07 09:11:59 +0300

    change comment model

--- models_app/models/comment/models.py
+++ models_app/models/comment/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ..photo.models import Photo
+from ..base.models import BaseModel
 
 
-class Comment(models.Model):
+class Comment(BaseModel):
     content = models.CharField(max_length=200)
     parent = models.ForeignKey('self', null=True, blank=True,
                                related_name='subcomments',
@@ -13,6 +14,11 @@ class Comment(models.Model):
                               on_delete=models.CASCADE)
     author = models.ForeignKey(User, related_name='comments',
                                on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = 'Comment'
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
 
     def __str__(self):
         return self.content
