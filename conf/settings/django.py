6141115c03602ae93ccb0bbbd05872f47907933a
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-09 09:31:28 +0300

    add settings for media and auth

--- conf/settings/django.py
+++ conf/settings/django.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'models_app',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'service_objects'
+    'service_objects',
+    'adrf'
 ]
 
 MIDDLEWARE = [
@@ -121,12 +124,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
 
 CORS_ORIGIN_WHITELIST = [
 
