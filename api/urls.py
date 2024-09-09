fef5b1a6ed9d3a3c4669bdef7a75d702b6aae41a
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-09 20:56:32 +0300

    change api urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,12 +4,15 @@ from django.conf.urls.static import static
 from .views.registration.views import RegisterView
 from .views.authorization.views import LoginView
 from .views.photo.views import PhotoListPublicView, PhotoListForAuthorView
+from .views.user.views import UserProfileView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('photos/', PhotoListPublicView.as_view(), name="photo"),
-    path('users-photos/', PhotoListForAuthorView.as_view(), name='users-photo')
+    path('users-photos/', PhotoListForAuthorView.as_view(),
+         name='users-photo'),
+    path('user-info/', UserProfileView.as_view(), name="user")
 ]
 
 if settings.DEBUG:
