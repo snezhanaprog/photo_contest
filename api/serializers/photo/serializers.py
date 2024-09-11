ff09d049e143a0b92a4c681731af2fca4481fec1
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-11 09:56:10 +0300

    delete create  method in photo serializer

--- api/serializers/photo/serializers.py
+++ api/serializers/photo/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from models_app.models.photo.models import Photo
-from api.services.photo.create import CreatePhotoService
 
 
 class PhotoSerializer(serializers.ModelSerializer):
@@ -8,9 +7,6 @@ class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = '__all__'
-
-    def create(self, validated_data):
-        return CreatePhotoService.create_photo(**validated_data)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
