3ceddbfb8fb27bf0d6412af27099b5895a214d8c
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-15 12:20:13 +0300

    change voice views

--- api/views/voice/views.py
+++ api/views/voice/views.py
@@ -1,40 +1,33 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from api.serializers.voice.serializers import VoiceSerializer
 from rest_framework.permissions import IsAuthenticated
 from api.services.voice.delete import DeleteVoiceService
 from api.services.voice.create import CreateVoiceService
-from api.services.voice.status import StatusVoiceService
+from utils.django_service_objects.service_objects.services import ServiceOutcome  # noqa: E501
 
 
-class ChangeStatusVoiceView(APIView):
+class CreateVoiceView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        if request.data['status']:
-            service = DeleteVoiceService(data=request.data)
-            status_voice = False
-        else:
-            service = CreateVoiceService(data=request.data)
-            status_voice = True
-        try:
-            voice = service.process(author=self.request.user)
-            print(voice)
-            return Response(
-                {'status': str(status_voice)}, status=status.HTTP_201_CREATED
-            )
-        except Exception as e:
-            print("Ошибка обработки голоса:", e)
-            ValueError("Ошибка обработки голоса:", e)
-            return Response({'error': e}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class StatusVoiceView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        service = StatusVoiceService(data=request.data)
-        status_voice = service.process(author=self.request.user)
-        return Response(
-            {'success': status_voice}, status=status.HTTP_201_CREATED
+        outcome = ServiceOutcome(
+            CreateVoiceService,
+            {**request.data, "author": request.user}
         )
+        return Response(
+            VoiceSerializer(outcome.result).data,
+            status=status.HTTP_200_OK
+            )
+
+
+class DeleteVoiceView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        ServiceOutcome(
+            DeleteVoiceService,
+            {**request.data, "author": request.user}
+        )
+        return Response(status=status.HTTP_204_NO_CONTENT)
