6a55a07d53e99b361b94d67e6a8b6d6aebf04d1d
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-15 12:23:38 +0300

    change api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,41 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views.registration.views import RegisterView
-from .views.authorization.views import LoginView
+from .views.user.views import UserProfileView, RegisterView, LoginView
 from .views.photo.views import ListPublicPhotoView, ListAuthorPhotoView
 from .views.photo.views import RetrievePhotoView, UploadPhotoView
-from .views.user.views import UserProfileView
-from .views.voice.views import StatusVoiceView
-from .views.voice.views import ChangeStatusVoiceView
+from .views.photo.views import UpdatePhotoView, DeletePhotoView
+from .views.voice.views import CreateVoiceView, DeleteVoiceView
+from .views.comment.views import ListCommentView, RetrieveCommentView
+from .views.comment.views import CreateCommentView
+from .views.comment.views import DeleteCommentView, UpdateCommentView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('user-info/', UserProfileView.as_view(), name="user"),
+
     path('photos/', ListPublicPhotoView.as_view(), name="photos"),
     path('users-photos/', ListAuthorPhotoView.as_view(),
          name='users-photo'),
-    path('user-info/', UserProfileView.as_view(), name="user"),
-    path('photos/<int:photo_id>/', RetrievePhotoView.as_view(), name="photo"),
+    path('photo/<int:id>/', RetrievePhotoView.as_view(), name="photo"),
     path('upload-photo/', UploadPhotoView.as_view(), name='upload-photo'),
-    path('status-voice/', StatusVoiceView.as_view(), name="status-voice"),
-    path('change-voice/', ChangeStatusVoiceView.as_view(), name="change-voice")
+    path('delete-photo/<int:id>/', DeletePhotoView.as_view(),
+         name="delete-photo"),
+    path('update-photo/<int:id>', UpdatePhotoView.as_view(),
+         name='update-photo'),
+
+    path('delete-voice/', DeleteVoiceView.as_view(), name="delete-voice"),
+    path('create-voice/', CreateVoiceView.as_view(), name="create-voice"),
+
+    path('comments/', ListCommentView.as_view(), name="comments"),
+    path('comment/<int:id>/', RetrieveCommentView.as_view(), name="comment"),
+    path('create-comment/', CreateCommentView.as_view(),
+         name='create-comment'),
+    path('delete-comment/<int:id>/', DeleteCommentView.as_view(),
+         name="delete-comment"),
+    path('update-comment/<int:id>', UpdateCommentView.as_view(),
+         name='update-comment'),
 ]
 
 if settings.DEBUG:
