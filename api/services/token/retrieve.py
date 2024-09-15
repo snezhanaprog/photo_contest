1bb3927063d512cdd807b0aa1dcf6c0433bcc0a8
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-15 12:07:15 +0300

    add token services

---
+++ api/services/token/retrieve.py
@@ -0,0 +1,30 @@
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django import forms
+from utils.django_service_objects.service_objects.services import ServiceWithResult  # noqa: E501
+
+
+class RetrieveTokenService(ServiceWithResult):
+    username = forms.CharField(max_length=50, required=True)
+    password = forms.CharField(max_length=50, required=True)
+
+    def process(self):
+        self.result, _ = self._token
+        return self
+
+    @property
+    def _token(self):
+        try:
+            return Token.objects.get_or_create(user=self._user)
+        except Exception:
+            return None
+
+    @property
+    def _user(self):
+        try:
+            return authenticate(
+                username=self.cleaned_data['username'],
+                password=self.cleaned_data['password']
+            )
+        except Exception:
+            return None
