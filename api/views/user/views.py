4b3ae80da027f3eda47c1a01d30462d732a7d8f2
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-15 12:14:25 +0300

    change user views

--- api/views/user/views.py
+++ api/views/user/views.py
@@ -1,29 +1,51 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.services.user.element import ElementUserService
+from api.services.user.retrieve import RetrieveUserService
 from rest_framework.permissions import IsAuthenticated
+from utils.django_service_objects.service_objects.services import ServiceOutcome  # noqa: E501
+from rest_framework import status
+from api.services.token.retrieve import RetrieveTokenService
+from api.services.user.create import CreateUserService
 
 
 class UserProfileView(APIView):
     permission_classes = [IsAuthenticated,]
 
     def get(self, request):
-        service = ElementUserService(data={'username': request.user.username})
-
-        if service.is_valid():
-            user_profile = service.process()
-            if user_profile['profile'].avatar:
-                avatar_url = user_profile['profile'].avatar.url
-            else:
-                avatar_url = None
-            return Response({
+        outcome = ServiceOutcome(
+            RetrieveUserService,
+            {'username': request.user.username}
+        )
+        if outcome.result['profile'].avatar:
+            avatar_url = outcome.result['profile'].avatar.url
+        else:
+            avatar_url = None
+        return Response({
                 'user': {
-                    'username': user_profile['user'].username,
-                    'email': user_profile['user'].email,
+                    'username': outcome.result['user'].username,
+                    'email': outcome.result['user'].email,
                 },
                 'profile': {
                     'avatar': avatar_url,
                 }
             })
-        else:
-            return Response({'error': service.errors}, status=400)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        ServiceOutcome(CreateUserService, request.data)
+        outcome = ServiceOutcome(RetrieveTokenService, request.data)
+        print(outcome.__dict__)
+        return Response(
+            {'auth_token': str(outcome.result)},
+            status=status.HTTP_201_CREATED
+        )
+
+
+class LoginView(APIView):
+    def post(self, request):
+        outcome = ServiceOutcome(RetrieveTokenService, request.data)
+        return Response(
+            {'auth_token': str(outcome.result)},
+            status=status.HTTP_200_OK
+        )
