246041d8206bc358f073d47f22c7034532c575ad
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-09-15 12:12:30 +0300

    change photo serializers

--- api/serializers/photo/serializers.py
+++ api/serializers/photo/serializers.py
@@ -1,17 +1,36 @@
 from rest_framework import serializers
 from models_app.models.photo.models import Photo
+from models_app.models.voice.models import Voice
 from django.contrib.auth.models import User
 
 
 class PhotoSerializer(serializers.ModelSerializer):
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Photo
         fields = '__all__'
 
+    def is_liked_photo(self, author, photo):
+        try:
+            voice = Voice.objects.get(
+                author=author,
+                associated_photo=photo
+                )
+            if voice:
+                return True
+        except Exception:
+            return False
+
     def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['image'] = instance.get_absolute_url()
-        author = User.objects.get(id=representation['author'])
-        representation['author'] = author.username
-        return representation
+        repr = super().to_representation(instance)
+        repr['image'] = instance.get_absolute_url()
+
+        author_obj = User.objects.get(id=repr['author'])
+        photo_obj = Photo.objects.get(id=repr['id'])
+
+        repr['author'] = author_obj.username
+        repr['is_liked'] = self.is_liked_photo(
+            author=author_obj, photo=photo_obj
+        )
+        return repr
